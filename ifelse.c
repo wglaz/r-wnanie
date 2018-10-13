aae7ad5c5856870e97ffc9c65c4033445fdcf6bd
wglaz <wojciu6666@gmail.com>
2018-10-13 09:15:15 +0200

    zmiana wyglądu przeze dodanie funkcji2

--- ifelse.c
+++ ifelse.c
@@ -3,6 +3,8 @@
 
 void wczytanie_danych(double *a, double *b, double *c);
 
+void podaje_x1_i_x2(double a, double b, double x1, double x2, double delta);
+
 #include <math.h>
 
 int main() {
@@ -29,12 +31,15 @@ int main() {
         printf("Rozwiązanie %lf", x1);
     }
     if(delta > 0){
-        //a = sqrt (c);
-        x1 = (-b-sqrt(delta))/2*a;
-        x2 = (-b+sqrt(delta))/2*a;
-        printf("Rozwiązanie x1=%lf, x2=%lf\n", x1, x2);
+        podaje_x1_i_x2(a, b, x1, x2, delta);
     }
     return 0;
+}
+
+void podaje_x1_i_x2(double a, double b, double x1, double x2, double delta) {//a = sqrt (c);
+    x1 = (-b-sqrt(delta))/2*a;
+    x2 = (-b+sqrt(delta))/2*a;
+    printf("Rozwiązanie x1=%lf, x2=%lf\n", x1, x2);
 }
 
 void wczytanie_danych(double *a, double *b, double *c) {
